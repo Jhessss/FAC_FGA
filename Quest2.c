5f9e906a8ac9204405a705268a24a862143333d8
Jhéssica Isabel Coelho <beelcs@hotmail.com>
2017-08-23 22:07:49 -0300

    Criado código fonte da questão 2

---
+++ Quest2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main( int argc, char *argv[ ])
+{
+  int cont;
+    for(cont=0; cont < argc; cont++)
+      printf("%d parametro: %s\n", cont, argv[cont]);
+    getchar();
+    return 0;
+}
