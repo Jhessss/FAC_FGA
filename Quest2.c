b38bbec6c13aa6f480ca8bd9b80a25dfb217602a
Jhéssica Isabel Coelho <beelcs@hotmail.com>
2017-08-24 09:56:50 -0300

    Versão Final

--- Quest2.c
+++ Quest2.c
@@ -2,10 +2,11 @@
 
 int main( int argc, char *argv[ ])
 {
+  printf("# de parametros: %i\nNome do executavel: %s\n", argc -1, argv[0]);
+
   int cont;
-    for(cont=0; cont < argc; cont++)
-      printf("%d parametro: %s\n", cont, argv[cont]);
-    getchar();
+    for(cont=1; cont < argc; cont++)
+      printf("Parametro: #%i: %s\n", cont, argv[cont]);
 
     return 0;
 }
